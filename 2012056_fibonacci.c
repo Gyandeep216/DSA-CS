18fa4e9510824e6497b6473e54332fd94aaa1ac5
Gyandeep216 <81816965+Gyandeep216@users.noreply.github.com>
2021-09-30 11:50:15 +0530

    Update 2012056_fibonacci.c

--- 2012056_fibonacci.c
+++ 2012056_fibonacci.c
@@ -5,19 +5,6 @@
 
 #include<stdio.h>
 
-//This is the function that conatains the basic logic to find the nth Fibonacci number.
-void fibonacci(int n,char first_num[],char second_num[],char result[]){
-    
-    second_num[9999] = '1';
-    
-    for (int i = 2; i <= n; i++){
-     stringAdd(first_num,second_num,result);
-     stringShift(second_num,first_num);
-     stringShift(result,second_num);
-    }
-   
-}
-
 //This function adds numbers stored as strings in the character arrays.
 void stringAdd(char first_num[], char second_num[], char result[]){
     
@@ -37,6 +24,19 @@ void stringShift(char s1[], char s2[]){
         s2[i] = s1[i];
     }
     
+}
+
+//This is the function that conatains the basic logic to find the nth Fibonacci number.
+void fibonacci(int n,char first_num[],char second_num[],char result[]){
+    
+    second_num[9999] = '1';
+    
+    for (int i = 2; i <= n; i++){
+     stringAdd(first_num,second_num,result);
+     stringShift(second_num,first_num);
+     stringShift(result,second_num);
+    }
+   
 }
 
 //Declaration of character arrays.
